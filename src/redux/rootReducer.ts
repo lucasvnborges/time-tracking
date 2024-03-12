ed2d3e314b1fb7d736d4a7252d6f9802e14b3b48
Lucas Borges <lucasvnborges@gmail.com>
2024-03-12 19:15:22 -0300

    chore: redux initial config

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,30 @@
+import { combineReducers } from "redux";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => ({
+  getItem() {
+    return Promise.resolve(null);
+  },
+  setItem(_key: any, value: any) {
+    return Promise.resolve(value);
+  },
+  removeItem() {
+    return Promise.resolve();
+  },
+});
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+const rootPersistConfig = {
+  key: "root",
+  storage,
+  keyPrefix: "redux-",
+  whitelist: [],
+};
+
+const rootReducer = combineReducers({});
+
+export { rootPersistConfig, rootReducer };
