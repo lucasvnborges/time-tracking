90c73dcd18b1d1540d0364eb24c37cca0c77ad17
Lucas Borges <lucasvnborges@gmail.com>
2024-03-13 19:16:32 -0300

    feat: create first page

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Page from "@components/Page";
+
+export default function HomePage() {
+  return (
+    <Page title="The starting point for your next project">
+      <p>The starting point for your next project</p>
+    </Page>
+  );
+}
